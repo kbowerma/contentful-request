cc314ee95497f51e1491204e073dfb558f80fe17
Kyle Bowerman <kyle.bowerman@gmail.com>
2015-10-05 15:05:09 -0500

    test push

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ var ROOT_URL = "https://cdn.contentful.com/spaces";
 var app = express();
 
 app.get("/", function(req, res) {
-    res.end("Check out /api/posts and /api/authors! \n this is the staging branch");
+    res.end("Check out /api/posts and /api/authors! \n this is the staging branch \n push2 test");
 });
 
 app.get("/api/posts", function(req, res) {
